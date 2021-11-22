5e1a36ebcc26ec02de28d2087b69e9d48244c465
Nana <realyeolly@gmail.com>
2021-11-22 21:07:08 +0500

    Interface commit 1.3

--- src/ru/list/real_pcy/homework_oop/Manager.java
+++ src/ru/list/real_pcy/homework_oop/Manager.java
@@ -12,6 +12,7 @@ public class Manager implements Worker {
         this.age = age;
     }
 
+    // region setter/getter
     @Override
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
@@ -27,9 +28,8 @@ public class Manager implements Worker {
         return name;
     }
 
-    @Override
-    public int getAge() {
-        return age;
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
@@ -43,6 +43,16 @@ public class Manager implements Worker {
     }
 
     @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    // endregion
+
+    @Override
     public void salaryValidate(double salary) throws WrongSalaryException {
         if (isAgeGreaterThenSalary(salary)) {
             throw new WrongSalaryException("Age is greater then salary");
@@ -52,55 +62,4 @@ public class Manager implements Worker {
         }
         System.out.println(this.name + " has a salary that equals " + this.salary);
     }
-
-//    // region setter/getter
-//    @Override
-//    public void setCompanyName(String companyName) {
-//        this.companyName = companyName;
-//    }
-//
-//    @Override
-//    public String getCompanyName(){
-//        return companyName;
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    @Override
-//    public void setSalary(double salary) {
-//        this.salary = salary;
-//    }
-//
-//    @Override
-//    public double getSalary() {
-//        return salary;
-//    }
-//
-//    @Override
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//    // endregion
-//
-//    @Override
-//    public void salaryValidate(double salary) throws WrongSalaryException {
-//        if (isAgeGreaterThenSalary(salary)) {
-//            throw new WrongSalaryException("Age is greater then salary");
-//        }
-//        if (isSalaryNotSet()) {
-//            throw new WrongSalaryException("Salary isn't set!");
-//        }
-//        System.out.println(this.name + " has a salary that equals " + this.salary);
-//    }
 }
--- src/ru/list/real_pcy/homework_oop/Worker.java
+++ src/ru/list/real_pcy/homework_oop/Worker.java
@@ -4,36 +4,42 @@ public interface Worker {
 
     /**
      * Метод устанавливает имя компании.
+     *
      * @param name
      */
     void setCompanyName(String name);
 
     /**
      * Метод возвращает имя компании
+     *
      * @return имя компании
      */
     String getCompanyName();
 
     /**
      * Метод возвращает имя работника.
+     *
      * @return имя сотрудника
      */
     String getName();
 
     /**
      * Возвращает возраст сотрудника
+     *
      * @return возраст сотрудника
      */
     int getAge();
 
     /**
      * Метод устанавливает зарплату.
+     *
      * @param salary
      */
     void setSalary(double salary);
 
     /**
      * Возвращает зарплату сотрудника
+     *
      * @return зарплата сотрудника
      */
     double getSalary();
@@ -47,11 +53,12 @@ public interface Worker {
 
     /**
      * Метод проверяет зарплату работника
+     *
      * @param salary
      */
     void salaryValidate(double salary) throws WrongSalaryException;
 
-    default boolean isSalaryNotSet(){
+    default boolean isSalaryNotSet() {
         return getSalary() <= 0;
     }
 
